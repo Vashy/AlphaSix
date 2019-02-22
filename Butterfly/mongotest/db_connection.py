4bb9bbdf7fb76350893850651ac5f815ef3912b4
Timoty <timoty.granziero@gmail.com>
2019-02-22 14:22:28 +0100

    Terminato populate.py

--- Butterfly/mongotest/db_connection.py
+++ Butterfly/mongotest/db_connection.py
@@ -3,22 +3,35 @@ import pymongo
 
 class DBConnection(object):
     def __init__(self, db, server='localhost', port=27017):
-        print('Opening connection ...')
+        print('Apertura connessione ...')
         self._client = pymongo.MongoClient(server, port)
+        print('Connessione stabilita.')
         self._db = self._client[db]
 
+    # Entrata nel costrutto with
     def __enter__(self):
-        # print('Opening connection ...')
         return self
 
+    # Uscita dal costrutto with
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # print('Closing connection ...')
         self.close()
 
-    # @property
+    def drop_collections(self, *collections):
+        """Elimina le collezioni passate come argomenti,
+        solo se presenti.
+        """
+        for collection in collections:
+            if collection in self.db.list_collection_names():
+                self.db.drop_collection(collection)
+                print(f'Dropped collection {collection}')
+
+    @property
     def db(self):
         return self._db
 
     def close(self):
-        print('Closing connection ...')
+        """Chiude la connessione col client mongo.
+        """
+        print('Chiusura connessione ...')
         self._client.close()
+        print('Connessione chiusa.')
--- Butterfly/mongotest/populate.py
+++ Butterfly/mongotest/populate.py
@@ -8,31 +8,72 @@ from mongotest.db_connection import DBConnection
 
 
 with DBConnection('myDB') as client:
-    # print(client._db.collection_names())
+    print(client._db.collection_names())
 
-    db = client.db()
+    db = client.db
     with open(Path(__file__).parent / 'db.json') as f:
         data = json.load(f)
 
+    client.drop_collections('users', 'projects', 'topics')
+
     users_json = data['users']
+    users = db.users  # Prepara la collezione users
 
-    if 'users' in db.list_collection_names():
-        db.drop_collection('users')
+    projects_json = data['projects']
+    projects = db.projects  # Prepara la collezione projects
 
-    users = db.users
-    print(users.find())
-    result = db.users.create_index(
-        [('telegram', pymongo.ASCENDING)],
-        unique=True
+    topics_json = data['topics']
+    topics = db.topics  # Prepara la collezione topics
+
+    # Popola la collezione users da db.json
+    for user in users_json:
+        if user['telegram'] is None and user['email'] is None:
+            print(f'User {timoty}')
+        # Se telegram è già presente
+        if users.find_one({'telegram': user['telegram']}):
+            print(f'Username {user["telegram"]} già presente')
+
+        # Se email è già presente
+        elif users.find_one({'email': user['email']}):
+            print(f'Email {user["email"]} già presente')
+
+        # Via libera all'aggiunta al DB
+        else:
+            result = users.insert_one(user)
+            print(result.inserted_id)
+
+    # Rende unique l'url dei progetti
+    projects.create_index(
+        [('url', pymongo.ASCENDING)],
+        unique=True,
     )
 
-    result = db.users.insert_many(users_json)
-    # print(result)
+    # Popola la collezione projects da db.json
+    for project in projects_json:
+        try:  # Tenta l'aggiunta del progetto
+            result = projects.insert_one(project)
+            # Stampa l'id se l'aggiunta avviene con successo
+            print(result.inserted_id)
+        # url già presente, segnala l'errore
+        # e prosegue con il prossimo documento
+        except pymongo.errors.DuplicateKeyError as err:
+            print(err)
 
-    for user in users.find({'_id': {'$gt': 122}}):
-        print(user)
+    # Rende unica la coppia label-project di topics
+    topics.create_index(
+        [('label', pymongo.ASCENDING),
+            ('project', pymongo.ASCENDING)],
+        unique=True,
+    )
 
+    # Popola la collezione topics da db.json
+    for topic in topics_json:
+        try:  # Tenta l'aggiunta del topic al DB
+            result = topics.insert_one(topic)
+            # Stampa l'id se l'aggiunta avviene con successo
+            print(result.inserted_id)
+        except pymongo.errors.DuplicateKeyError as err:
+            print(err)
 
-
-    # collection = db
-    # users = db.users
+    # for user in users.find({}):
+    #     pprint.pprint(user)
