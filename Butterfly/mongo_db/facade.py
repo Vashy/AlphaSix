9bd94c7c0870349ddc1dd129ee03dac30c84a555
Matteo Marchiori <matteo.marchiori97@gmail.com>
2019-03-28 14:49:27 +0100

    continuo facade

--- Butterfly/mongo_db/facade.py
+++ Butterfly/mongo_db/facade.py
@@ -5,31 +5,31 @@ class MongoFacade:
         self._mongo_projects = mongo_projects
 
     def insert_user(self, **fields):
-        pass
-        
+        self._mongo_users.create(fields)
+
     def delete_user(self, user: str):
-        pass
-    
+        self._mongo_users.delete(user)
+
     def update_user_name(self, user: str, name: str):
-        pass
-    
+        self._mongo_users.update_user_name(user, name)
+
     def update_user_surname(self, user: str, surname: str):
-        pass
-        
+        self._mongo_users.update_user_surname(user, surname)
+
     def update_user_telegram(self, user: str, telegram: str):
-        pass
+        self._mongo_users.update_user_telegram(user, telegram)
 
     def update_user_email(self, user: str, email: str):
-        pass
-        
+        self._mongo_users.update_user_email(user, email)
+
     def insert_user_project(self, user: str, project: str):
-        pass
-        
+        self._mongo_users.insert_user_project(user, project)
+
     def delete_user_project(self, user: str, project: str):
-        pass
-    
+        self._mongo_users.delete_user_project(user, project)
+
     def insert_project(self, project: str):
-        pass
-        
-    def delete_project(self, url: str):
-        pass
+        self._mongo_projects.create(project)
+
+    def delete_project(self, project: str):
+        self._mongo_projects.delete(project)
