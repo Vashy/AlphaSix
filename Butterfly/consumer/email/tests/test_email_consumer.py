8e770efd91c5b95065bb4c4cf594212d4a1963a0
Timoty <timoty.granziero@gmail.com>
2019-03-29 20:43:31 +0100

    wip test consumer mail

--- Butterfly/consumer/email/tests/test_email_consumer.py
+++ Butterfly/consumer/email/tests/test_email_consumer.py
@@ -1,33 +1,13 @@
 from unittest.mock import MagicMock, patch
 
-from consumer.email.creator import EmailConsumerCreator, EmailConsumer
+from consumer.email.consumer import EmailConsumer
 
-
-def test_instantiate():
-    creator = EmailConsumerCreator()
-    assert creator.topic == 'email'
-    assert creator.topic != 'telegram'
-
-    kafka_mock = MagicMock()
-    consumer = creator.instantiate(kafka_mock)
-
-    assert isinstance(consumer, EmailConsumer)
-
-@patch('consumer.creator.Path')
-@patch('consumer.creator.KafkaConsumer', autospec=True)
-@patch('consumer.creator.json')
-def test_create(
-        json,
-        kafka,
-        path,
+@patch('consumer.email.consumer.smtplib')
+def test_send(
+    smtplib,
 ):
-    json.load.return_value = {
-        'kafka': {
-            'consumer_timeout_ms': 1,
-        },
-        'another': 'value'
-    }
-    creator = EmailConsumerCreator()
+    kafka_mock = MagicMock()
+    consumer = EmailConsumer(kafka_mock, 'email')
 
-    consumer = creator.create()
-    assert isinstance(consumer, EmailConsumer)
+    smtplib.SMTPAuthenticationError = Exception
+    consumer.send('msg')
