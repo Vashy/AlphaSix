5c2fcfd52cc11f330a17ba9a5d643daabc1b52c2
LauraCameran <lauracameran@gmail.com>
2019-03-29 18:25:05 +0100

    Test in corso

--- Butterfly/gestore_personale/Processor.py
+++ Butterfly/gestore_personale/Processor.py
@@ -12,12 +12,12 @@ class Processor():
     # Metodo che chiama a ruota tutti quelli dopo per processare il messaggio
     # ex template_method
     def prepare_message(self) -> dict:
-        progetto = self.check_project()  # URL progetto
+        progetto = self._check_project()  # URL progetto
         obj = self._message['object_kind']  # Issue o push ecc
         # Dict di tutti gli utenti disponibili oggi nel progetto
         utenti_disponibili = self.get_involved_users()
         # Lista di tutti gli utenti interessati e disponibili
-        utenti_interessati = self.filter_users_by_topic(
+        utenti_interessati = self._filter_users_by_topic(
             utenti_disponibili, obj
         )
         # Se non c'è nessuno, vedo la persona di priorità
@@ -47,10 +47,32 @@ class Processor():
     #     return self.__mongofacade.get_users_project(project)
 
     def get_involved_users(self, project: str) -> list:
-        return self._mogofacade.get_users_available(project)
+        return self._mongofacade.get_users_available(project)
 
     # Metodo astratto che delega alle sottoclassi concrete
     # il filtraggio degli utenti in base a quelli iscritti ai topic
     @abstractmethod
     def _filter_users_by_topic(self, users: list, obj: str) -> list:
         pass
+
+    # Lista di tutti gli utenti disponibili e più interessati
+    def _select_users_more_interested(self, project: str) -> list:
+        return self.__mongofacade.get_users_max_priority(project)
+
+    # Crea la lista di contatti telegram a cui inviare il messaggio
+    def _get_telegram_contacts(self, users: list) -> list:
+        contacts = []
+        for user in users:
+            telegramID = self.__mongofacade.get_user_telegram(user)
+            if telegramID is not None:
+                contacts.append(telegramID)
+        return contacts
+
+    # Crea la lista di contatti mail a cui inviare il messaggio
+    def _get_email_contacts(self, users: list) -> list:
+        contacts = []
+        for user in users:
+            emailID = self.__mongofacade.get_user_email(user)
+            if emailID is not None:
+                contacts.append(emailID)
+        return contacts
