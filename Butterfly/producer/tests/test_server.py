7fc9cf0b16021b649fa6f731ed4828804d4d3a7b
Timoty <timoty.granziero@gmail.com>
2019-04-03 11:20:38 +0200

    Sistemati test

--- Butterfly/producer/tests/test_server.py
+++ Butterfly/producer/tests/test_server.py
@@ -3,50 +3,27 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 import producer
-from producer.server import FlaskServerCreator, FlaskServer
+from producer.server import FlaskServer
 
 
-@patch('producer.server.FlaskServer')
-@patch('producer.server.Flask')
-def test_flask_creator(flask_mock, server_mock):
-    mock = MagicMock()
-    with pytest.raises(AssertionError):
-        FlaskServerCreator(mock)
+# @patch('producer.server.Path')
+# @patch('producer.server.json')
+# def test_open_configs(json_mock, path_mock):
+#     path_mock.return_value = producer.server.Path()
+#     json_mock.load.return_value = {'gitlab': '5003'}
+#     val = producer.server._open_configs(path_mock())
 
-    mock.__class__ = producer.gitlab.creator.GitlabProducerCreator
-    server = FlaskServerCreator(mock)
-    app = server.initialize_app('gitlab')
-
-    flask_mock.assert_called_once()
-    server_mock.assert_called_once()
+#     assert 'gitlab' in val
+#     assert path_mock.called_once
 
 
-@patch('producer.server.Path')
-@patch('producer.server.json')
-def test_open_configs(json_mock, path_mock):
-    path_mock.return_value = producer.server.Path()
-    json_mock.load.return_value = {'gitlab': '5003'}
-    val = producer.server._open_configs(path_mock())
-
-    assert 'gitlab' in val
-    assert path_mock.called_once
-
-
-@patch('producer.server.json')
-def test_server(json_mock):
+def test_server():
     flask_mock = MagicMock()
     producer_mock = MagicMock()
-    json_mock.load.return_value = {
-        'gitlab': {
-            'ip': 'localhost',
-            'port': 5003,
-        }
-    }
 
     server = FlaskServer(flask_mock, producer_mock, 'gitlab')
     server.run(MagicMock())
 
-    json_mock.load.assert_called_once()
     flask_mock.run.assert_called_once()
 
 
